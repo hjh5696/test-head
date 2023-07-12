b679abd5f8d5d6bfe8869a3b820452867b0f93ae
Junhyeong Hong <hjh5696@gmail.com>
2023-07-12 12:50:16 +0100

    test4

--- src/app/create-schedule/page.tsx
+++ src/app/create-schedule/page.tsx
@@ -1,17 +1,25 @@
 'use client';
+import { useEffect } from 'react';
 // import { useSearchParams } from 'next/navigation';
-import { Translate } from '../_components/translation/Translate';
+// import { Translate } from '../_components/translation/Translate';
 import './create-schedule.css'
 
 const CreateSchedule = () => {
   // const searchParams = useSearchParams();
   // const type = searchParams?.get('type');
   // console.log(type)
+  // useTestHook()
   const type = 'match'
   return <div className={`${type === 'match' ? 'container-red' : 'container-green'}`}>
-    <Translate stringKey='general.cancel'></Translate>
+    {/* <Translate stringKey='general.cancel'></Translate> */}
     {'\ntest createSchedule'}
   </div>
 }
 
 export default CreateSchedule
+
+// const useTestHook = () => {
+//   useEffect(() => {
+//     console.log("test hook")
+//   }, [])
+// }
