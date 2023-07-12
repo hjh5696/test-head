cc389752b5cd74b66b4e67f2b0e5381130126fae
Junhyeong Hong <hjh5696@gmail.com>
2023-07-12 15:28:31 +0100

    dynamic render

--- src/app/create-schedule/page.tsx
+++ src/app/create-schedule/page.tsx
@@ -6,11 +6,10 @@ import './create-schedule.css'
 export const dynamic = 'force-dynamic'
 
 const CreateSchedule = () => {
-  // const searchParams = useSearchParams();
-  // const type = searchParams?.get('type');
-  // console.log(type)
+  const searchParams = useSearchParams();
+  const type = searchParams?.get('type');
+  console.log(type)
   
-  const type = 'match'
   return <div className={`${type === 'match' ? 'container-red' : 'container-green'}`}>
     {/* <Translate stringKey='general.cancel'></Translate> */}
     {'\ntest createSchedule'}
