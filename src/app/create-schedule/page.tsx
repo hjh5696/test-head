16a741b68e550986fbfc4ad38c76a91d71934c20
Junhyeong Hong <hjh5696@gmail.com>
2023-07-12 13:01:22 +0100

    test

--- src/app/create-schedule/page.tsx
+++ src/app/create-schedule/page.tsx
@@ -1,13 +1,14 @@
 'use client';
-import { useSearchParams, usePathname } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 // import { Translate } from '../_components/translation/Translate';
 import './create-schedule.css'
 
 const CreateSchedule = () => {
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
   // const type = searchParams?.get('type');
   // console.log(type)
-  console.log(searchParams)
+  const router = useRouter()
+  console.log(router)
   
   const type = 'match'
   return <div className={`${type === 'match' ? 'container-red' : 'container-green'}`}>
