1cb8efa18dc3eecc16ad0334abe23457630dafc0
Junhyeong Hong <hjh5696@gmail.com>
2023-07-12 15:41:05 +0100

    test

---
+++ src/app/create-schedule/_components/CreateSchedule.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { Suspense } from 'react';
+// import { Translate } from '../_components/translation/Translate';
+import './create-schedule.css'
+
+const CreateSchedule = () => {
+  const searchParams = useSearchParams();
+  const type = searchParams?.get('type');
+  console.log(type)
+  
+  return <div className={`${type === 'match' ? 'container-red' : 'container-green'}`}>
+    {/* <Translate stringKey='general.cancel'></Translate> */}
+    {'\ntest createSchedule'}
+  </div>
+}
+
+export default CreateSchedule
--- src/app/create-schedule/page.tsx
+++ src/app/create-schedule/page.tsx
@@ -1,19 +1,5 @@
-'use client';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { Suspense } from 'react';
-// import { Translate } from '../_components/translation/Translate';
-import './create-schedule.css'
-
-const CreateSchedule = () => {
-  const searchParams = useSearchParams();
-  const type = searchParams?.get('type');
-  console.log(type)
-  
-  return <div className={`${type === 'match' ? 'container-red' : 'container-green'}`}>
-    {/* <Translate stringKey='general.cancel'></Translate> */}
-    {'\ntest createSchedule'}
-  </div>
-}
+import { Suspense } from "react"
+import CreateSchedule from "./_components/CreateSchedule"
 
 const Container = () => {
   <Suspense fallback={<p>Loading feed...</p>}>
