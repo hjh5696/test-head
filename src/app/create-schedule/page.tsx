b0187c5aec134c1db5afb763e9e1b97915ff25cd
Junhyeong Hong <hjh5696@gmail.com>
2023-07-12 15:51:05 +0100

    test

--- src/app/create-schedule/page.tsx
+++ src/app/create-schedule/page.tsx
@@ -3,11 +3,11 @@ import CreateSchedule from "./_components/CreateSchedule"
 import './create-schedule.css'
 
 const Container = () => {
-  return <CreateSchedule></CreateSchedule>
+  // return <CreateSchedule></CreateSchedule>
   
-  // return <Suspense fallback={<p>Loading feed...</p>}>
-  //   <CreateSchedule></CreateSchedule>
-  // </Suspense>
+  return <Suspense fallback={<p>Loading feed...</p>}>
+    <CreateSchedule></CreateSchedule>
+  </Suspense>
 }
 
 export default Container
